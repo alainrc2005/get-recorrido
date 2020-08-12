4d0bb17940346d469952682957b1cdd64d01f271
alain.ramirez <alainrc2005@gmail.com>
2020-08-12 13:48:09 -0400

    API KEY

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    initGoogleMap () {
+      try {
+        console.info('[GET] initGoogleMap')
+        const googleMapScript = document.createElement('SCRIPT'),
+          url = 'https://maps.googleapis.com/maps/api/js?key=APIKEY'
+        googleMapScript.setAttribute('src', url)
+        googleMapScript.setAttribute('async', '')
+        googleMapScript.setAttribute('defer', '')
+        document.head.appendChild(googleMapScript)
+      } catch (e) {
+        console.error(e)
+        Error.call(this, 'Ha ocurrido un error instanciando GoogleMap')
+      }
+    }
+  },
+  mounted () {
+    this.initGoogleMap()
+  }
+}
+</script>
